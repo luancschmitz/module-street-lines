385223390012e554211d038f42b5fc07d7482c41
Luan Carlos <luan_c_schmitz@outlook.com>
2020-04-06 22:14:54 -0300

    primeiro commit

---
+++ Plugin/Block/Checkout/LayoutProcessor.php
@@ -0,0 +1,76 @@
+<?php
+namespace Luan\StreetLines\Plugin\Block\Checkout;
+
+use Magento\Checkout\Block\Checkout\LayoutProcessor as LayoutProcessorCore;
+
+class LayoutProcessor
+{
+    public function afterProcess(
+        LayoutProcessorCore $subject,
+        array $jsLayout
+    ) {
+        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
+        ['shippingAddress']['children']['shipping-address-fieldset']['children']['street'] = [
+            'component' => 'Magento_Ui/js/form/components/group',
+            //'label' => __('Street Address')
+            'required' => false, //mudamos para false caso tehamos desabilitado o label acima
+            'dataScope' => 'shippingAddress.street',
+            'provider' => 'checkoutProvider',
+            'sortOrder' => 0,
+            'type' => 'group',
+            'additionalClasses' => 'street',
+            'children' => [
+                [
+                    'label' => __('Street'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '0',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                [
+                    'label' => __('Number'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '1',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                [
+                    'label' => __('Neighborhood'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '2',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                [
+                    'label' => __('Complement'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '3',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => false, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+            ]
+        ];
+
+        return $jsLayout;
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+#Módulo StreetLines
+Módulo para adicionar os labels Rua, Número, Complemento e Bairro 
+no Checkout. Módulo baseado no tutorial abaixo:
+
+https://www.magenteiro.com/blog/magento-2/linhas-endereco-amigaveis-magento2/
+s
